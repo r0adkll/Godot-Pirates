46fdb36c64299f5b82436ea628efcfa66cc2632f
Drew Heavner <veedubusc@gmail.com>
2026-07-07 07:04:46 -0400

    Revert "Keep ship spawns clear of hostile detection ranges (#7)"
    This reverts commit 490a3e923f0ed718fd8f199f97cc3535b96f7a2d.

--- Multiplayer/Scripts/player_system.gd
+++ Multiplayer/Scripts/player_system.gd
@@ -18,7 +18,7 @@ func setup_players() -> void:
 	if multiplayer.is_server():
 		var ids: Array = Lobby.players.keys()
 		for player_id: int in ids:
-			var pos = SpawnSafety.pick_spawn_position(treasure_map.navigation, get_faction(player_id))
+			var pos = get_random_position()
 			spawn_player.rpc(player_id, pos)
 
 
@@ -94,5 +94,5 @@ func _on_player_state_changed(
 	new_state: BaseShip.State
 ) -> void:
 	if new_state == BaseShip.State.DEAD:
-		var new_position = SpawnSafety.pick_spawn_position(treasure_map.navigation, ship.faction)
+		var new_position = get_random_position()
 		spawn_player.rpc(ship.faction.id, new_position)
